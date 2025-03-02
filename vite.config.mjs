25b221d03b927c244657ff7912943c88edc42f3e
Sultan Sultan <sitexpressorg@gmail.com>
2025-03-02 18:58:31 +0300

    ggg

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dns from 'node:dns'
 
 dns.setDefaultResultOrder('ipv4first')
 
