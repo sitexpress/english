da70fe057560dab02dfc0348819f138d14076281
Sultan Sultan <sitexpressorg@gmail.com>
2025-03-02 18:56:14 +0300

    fix to ipv4

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+dns.setDefaultResultOrder('ipv4first')
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
@@ -9,7 +11,8 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
-  preview: {
-    port: 6001
+  server: {
+    host: 'localhost',
+    port: 8080
   }
 });
